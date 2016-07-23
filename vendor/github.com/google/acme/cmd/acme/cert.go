16c0bb286275f3e351c04e843ca5b17e5acc0ce3
Jeff Hodges <jeff@somethingsimilar.com>
2016-07-23 04:39:45 -0700

    vendor things

---
+++ vendor/github.com/google/acme/cmd/acme/cert.go
@@ -0,0 +1,228 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"path/filepath"
+	"time"
+
+	"github.com/google/acme"
+)
+
+var (
+	cmdCert = &command{
+		run:       runCert,
+		UsageLine: "cert [-c config] [-d url] [-s host:port] [-k key] [-expiry dur] [-bundle=true] [-manual=false] domain [domain ...]",
+		Short:     "request a new certificate",
+		Long: `
+Cert creates a new certificate for the given domain.
+It uses http-01 challenge to complete authorization flow.
+
+The certificate will be placed alongside key file, specified with -k argument.
+If the key file does not exist, a new one will be created.
+Default location for the key file is {{.ConfigDir}}/domain.key,
+where domain is the actually domain name provided as the command argument.
+
+By default the obtained certificate will also contain the CA chain.
+If this is undesired, specify -bundle=false argument.
+
+The -s argument specifies the address where to run local server
+for the http-01 challenge. If not specified, 127.0.0.1:8080 will be used.
+
+An alternative to local server challenge response may be specified as -manual,
+in which case instructions are displayed on the standard output.
+
+Default location of the config dir is
+{{.ConfigDir}}.
+		`,
+	}
+
+	certDisco   = defaultDiscoFlag
+	certAddr    = "127.0.0.1:8080"
+	certExpiry  = 365 * 12 * time.Hour
+	certBundle  = true
+	certManual  = false
+	certKeypath string
+)
+
+func init() {
+	cmdCert.flag.Var(&certDisco, "d", "")
+	cmdCert.flag.StringVar(&certAddr, "s", certAddr, "")
+	cmdCert.flag.DurationVar(&certExpiry, "expiry", certExpiry, "")
+	cmdCert.flag.BoolVar(&certBundle, "bundle", certBundle, "")
+	cmdCert.flag.BoolVar(&certManual, "manual", certManual, "")
+	cmdCert.flag.StringVar(&certKeypath, "k", "", "")
+}
+
+func runCert(args []string) {
+	if len(args) == 0 {
+		fatalf("no domain specified")
+	}
+	cn := args[0]
+	if certKeypath == "" {
+		certKeypath = filepath.Join(configDir, cn+".key")
+	}
+
+	// get user config
+	uc, err := readConfig()
+	if err != nil {
+		fatalf("read config: %v", err)
+	}
+	if uc.key == nil {
+		fatalf("no key found for %s", uc.URI)
+	}
+
+	// read or generate new cert key
+	certKey, err := anyKey(certKeypath, true)
+	if err != nil {
+		fatalf("cert key: %v", err)
+	}
+	// generate CSR now to fail early in case of an error
+	req := &x509.CertificateRequest{
+		Subject: pkix.Name{CommonName: cn},
+	}
+	if len(args) > 1 {
+		req.DNSNames = args
+	}
+	csr, err := x509.CreateCertificateRequest(rand.Reader, req, certKey)
+	if err != nil {
+		fatalf("csr: %v", err)
+	}
+
+	// perform discovery to get the new-cert URL
+	disco, err := acme.Discover(nil, string(certDisco))
+	if err != nil {
+		fatalf("discovery: %v", err)
+	}
+	// initialize acme client and start authz flow
+	// we only look for http-01 challenges at the moment
+	client := &acme.Client{Key: uc.key}
+	for _, domain := range args {
+		if err := authz(client, uc.Authz, domain); err != nil {
+			fatalf("%s: %v", domain, err)
+		}
+	}
+
+	// challenge fulfilled: get the cert
+	cert, curl, err := client.CreateCert(disco.CertURL, csr, certExpiry, certBundle)
+	if err != nil {
+		fatalf("cert: %v", err)
+	}
+	logf("cert url: %s", curl)
+	if cert == nil {
+		cert = pollCert(curl)
+	}
+	var pemcert []byte
+	for _, b := range cert {
+		b = pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: b})
+		pemcert = append(pemcert, b...)
+	}
+	certPath := sameDir(certKeypath, cn+".crt")
+	if err := ioutil.WriteFile(certPath, pemcert, 0644); err != nil {
+		fatalf("write cert: %v", err)
+	}
+}
+
+func authz(client *acme.Client, zurl, domain string) error {
+	z, err := client.Authorize(zurl, domain)
+	if err != nil {
+		return err
+	}
+	var chal *acme.Challenge
+	for _, c := range z.Challenges {
+		if c.Type == "http-01" {
+			chal = &c
+			break
+		}
+	}
+	if chal == nil {
+		return errors.New("no supported challenge found")
+	}
+
+	// respond to http-01 challenge
+	ln, err := net.Listen("tcp", certAddr)
+	if err != nil {
+		return fmt.Errorf("listen %s: %v", certAddr, err)
+	}
+	defer ln.Close()
+
+	if certManual {
+		// manual challenge response
+		tok := fmt.Sprintf("%s.%s", chal.Token, acme.JWKThumbprint(&client.Key.PublicKey))
+		file, err := challengeFile(domain, tok)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Copy %s to ROOT/.well-known/acme-challenge/%s of %s and press enter.\n",
+			file, chal.Token, domain)
+		var x string
+		fmt.Scanln(&x)
+	} else {
+		// auto, via local server
+		go http.Serve(ln, client.HTTP01Handler(chal.Token))
+	}
+
+	if _, err := client.Accept(chal); err != nil {
+		return fmt.Errorf("accept challenge: %v", err)
+	}
+	for {
+		a, err := client.GetAuthz(z.URI)
+		if err != nil {
+			logf("authz %q: %v\n", z.URI, err)
+		}
+		if a.Status == acme.StatusInvalid {
+			return fmt.Errorf("could not authorize for %s", domain)
+		}
+		if a.Status != acme.StatusValid {
+			// TODO: use Retry-After
+			time.Sleep(time.Duration(3) * time.Second)
+			continue
+		}
+		break
+	}
+	return nil
+}
+
+func pollCert(url string) [][]byte {
+	for {
+		b, err := acme.FetchCert(nil, url, certBundle)
+		if err == nil {
+			return b
+		}
+		d := 3 * time.Second
+		if re, ok := err.(acme.RetryError); ok {
+			d = time.Duration(re)
+		}
+		time.Sleep(d)
+	}
+}
+
+func challengeFile(domain, content string) (string, error) {
+	f, err := ioutil.TempFile("", domain)
+	if err != nil {
+		return "", err
+	}
+	_, err = fmt.Fprint(f, content)
+	if err1 := f.Close(); err1 != nil && err == nil {
+		err = err1
+	}
+	return f.Name(), err
+}
---
+++ vendor/github.com/google/acme/cmd/acme/reg.go
@@ -0,0 +1,78 @@
+// Copyright 2015 Google Inc. All Rights Reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"path/filepath"
+
+	"github.com/google/acme"
+)
+
+var (
+	cmdReg = &command{
+		run:       runReg,
+		UsageLine: "reg [-c config] [-gen] [-d url] [contact [contact ...]]",
+		Short:     "new account registration",
+		Long: `
+Reg creates a new account at a CA using the discovery URL
+specified with -d argument. The default value is {{.DefaultDisco}}.
+For more information about the discovery run acme help disco.
+
+Upon successful registration, a new config will be written to {{.AccountFile}}
+in the directory specified with -c argument. Default location of the config dir
+is {{.ConfigDir}}.
+If the config dir does not exist, it will be created.
+
+Contact arguments can be anything: email, phone number, etc.
+
+If -gen flag is not specified, and an account key does not exist, the command
+will exit with an error.
+
+See also: acme help account.
+		`,
+	}
+
+	regDisco = defaultDiscoFlag
+	regGen   bool
+)
+
+func init() {
+	cmdReg.flag.Var(&regDisco, "d", "")
+	cmdReg.flag.BoolVar(&regGen, "gen", regGen, "")
+}
+
+func runReg(args []string) {
+	key, err := anyKey(filepath.Join(configDir, accountKey), regGen)
+	if err != nil {
+		fatalf("account key: %v", err)
+	}
+	uc := &userConfig{
+		Account: acme.Account{Contact: args},
+		key:     key,
+	}
+
+	// perform discovery to get the reg url
+	urls, err := acme.Discover(nil, string(regDisco))
+	if err != nil {
+		fatalf("discovery: %v", err)
+	}
+	// do the registration
+	client := acme.Client{Key: uc.key}
+	if err := client.Register(urls.RegURL, &uc.Account); err != nil {
+		fatalf("%v", err)
+	}
+	// success
+	// TODO: ask user for agreement acceptance
+	if err := writeConfig(uc); err != nil {
+		errorf("write config: %v", err)
+	}
+}
