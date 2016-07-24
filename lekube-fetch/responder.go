5ad58337fe5e7dec63e1d040f4c680f627712c64
Jeff Hodges <jeff@somethingsimilar.com>
2016-07-24 03:02:55 -0700

    remove notifier

--- lekube-fetch/responder.go
+++ lekube-fetch/responder.go
@@ -12,16 +12,11 @@ import (
 	jose "gopkg.in/square/go-jose.v1"
 )
 
-type tokData struct {
-	body     []byte
-	notifier chan<- bool
-}
-
 type leResponder struct {
 	accountKeyThumbprint string // raw base64url encoded thumbprint
 
 	sync.Mutex
-	tokens map[string]tokData
+	bodies map[string][]byte
 }
 
 func newLEResponser(accountPubKey *rsa.PublicKey) (*leResponder, error) {
@@ -33,7 +28,7 @@ func newLEResponser(accountPubKey *rsa.PublicKey) (*leResponder, error) {
 	thumbprintB64 := base64.RawURLEncoding.EncodeToString(thumbprint)
 	lr := &leResponder{
 		accountKeyThumbprint: thumbprintB64,
-		tokens:               make(map[string]tokData),
+		bodies:               make(map[string][]byte),
 	}
 	return lr, nil
 }
@@ -41,7 +36,7 @@ func newLEResponser(accountPubKey *rsa.PublicKey) (*leResponder, error) {
 const acmePath = "/.well-known/acme-challenge/"
 
 func (lr *leResponder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// FIXME remove
+	// FIXME add verbose
 	log.Printf("responder received %s", r.URL.Path)
 	if !strings.HasPrefix(r.URL.Path, acmePath) {
 		http.Error(w, "Not Found", http.StatusNotFound)
@@ -50,36 +45,24 @@ func (lr *leResponder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	token := r.URL.Path[len(acmePath):len(r.URL.Path)]
 	ok := false
 	lr.Lock()
-	tokData, ok := lr.tokens[token]
+	body, ok := lr.bodies[token]
 	lr.Unlock()
 	if !ok {
-		log.Printf("didn't find %#v in map", token)
 		http.Error(w, "Not Found", http.StatusNotFound)
 		return
 	}
-	w.Write(tokData.body)
-	select {
-	case tokData.notifier <- true:
-	default:
-		// In case we've not pulled the last notification, just fall
-		// through. This assumes notifier has a buffer of 1.
-	}
+	w.Write(body)
 }
 
 func (lr *leResponder) AddAuthorization(token string) {
 	ka := token + "." + lr.accountKeyThumbprint
-	notifier := make(chan bool, 1)
 	lr.Lock()
-	log.Printf("adding for real %#v, and body %#v", token, string(ka))
-	lr.tokens[token] = tokData{
-		body:     []byte(ka),
-		notifier: notifier,
-	}
-	lr.Unlock()
+	defer lr.Unlock()
+	lr.bodies[token] = []byte(ka)
 }
 
 func (lr *leResponder) Reset() {
 	lr.Lock()
 	defer lr.Unlock()
-	lr.tokens = make(map[string]tokData)
+	lr.bodies = make(map[string][]byte)
 }
