c054d680346ee9d54232f546ec1743d117181d45
Jeff Hodges <jeff@somethingsimilar.com>
2016-10-17 01:38:37 -0700

    ugh

--- config.go
+++ config.go
@@ -115,6 +115,7 @@ func (cl *confLoader) Watch() *allConf {
 			prevErr = err
 			recordError(loadConfigStage, "unable to load config file in watch goroutine: %s", err)
 		}
+		log.Printf("sleeping until %s", next)
 		time.Sleep(next.Sub(start))
 	}
 }
