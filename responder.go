50d4703cc6cb3775da5a2678a585cb814c97eccf
Jeff Hodges <jeff@somethingsimilar.com>
2016-12-25 02:52:10 -0500

    include domain information in responder

--- responder.go
+++ responder.go
@@ -16,7 +16,12 @@ type leResponder struct {
 	accountKeyThumbprint string // raw base64url encoded thumbprint
 
 	sync.Mutex
-	bodies map[string][]byte
+	bodies map[string]responseInfo
+}
+
+type responseInfo struct {
+	body   []byte
+	domain string
 }
 
 func newLEResponser(accountPubKey *rsa.PublicKey) (*leResponder, error) {
@@ -28,7 +33,7 @@ func newLEResponser(accountPubKey *rsa.PublicKey) (*leResponder, error) {
 	thumbprintB64 := base64.RawURLEncoding.EncodeToString(thumbprint)
 	lr := &leResponder{
 		accountKeyThumbprint: thumbprintB64,
-		bodies:               make(map[string][]byte),
+		bodies:               make(map[string]responseInfo),
 	}
 	return lr, nil
 }
@@ -52,26 +57,26 @@ func (lr *leResponder) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	token := r.URL.Path[len(acmePath):len(r.URL.Path)]
 	ok := false
 	lr.Lock()
-	body, ok := lr.bodies[token]
+	info, ok := lr.bodies[token]
 	lr.Unlock()
 	if !ok {
-		log.Printf("responder received missing token path %s", r.URL.Path)
+		log.Printf("responder received unknown token path %s", r.URL.Path)
 		http.Error(w, "Not Found", http.StatusNotFound)
 		return
 	}
-	log.Printf("responder received known path %s", r.URL.Path)
-	w.Write(body)
+	log.Printf("responder received known path (for domain %s) %s", info.domain, r.URL.Path)
+	w.Write(info.body)
 }
 
-func (lr *leResponder) AddAuthorization(token string) {
+func (lr *leResponder) AddAuthorization(domain, token string) {
 	ka := token + "." + lr.accountKeyThumbprint
 	lr.Lock()
 	defer lr.Unlock()
-	lr.bodies[token] = []byte(ka)
+	lr.bodies[token] = responseInfo{body: []byte(ka), domain: domain}
 }
 
 func (lr *leResponder) Reset() {
 	lr.Lock()
 	defer lr.Unlock()
-	lr.bodies = make(map[string][]byte)
+	lr.bodies = make(map[string]responseInfo)
 }
