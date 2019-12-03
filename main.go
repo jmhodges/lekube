0f93274809e59665240c46bfb4a9804d7edf16c1
Jeff Hodges <jeff@somethingsimilar.com>
2019-12-02 20:34:24 -0800

    i dunno try pod

--- main.go
+++ main.go
@@ -117,7 +117,7 @@ func main() {
 			ProjectID:    creds.ProjectID,
 			MetricPrefix: "lekube",
 			Resource: &monitoredres.MonitoredResource{
-				Type: "k8s_container",
+				Type: "k8s_pod",
 				Labels: map[string]string{
 					"project_id":     projID,
 					"location":       location,
